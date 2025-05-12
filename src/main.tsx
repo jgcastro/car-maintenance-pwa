a2a0a566f2f7b73492314a1b443555e6538316ee
Juan Castro <sempai211@gmail.com>
2025-05-12 00:21:13 -0600

    Add Suspense and the Mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
+import React, { StrictMode, Suspense } from 'react'
+import ReactDOM from 'react-dom/client'
+import { MantineProvider, Loader, Center } from '@mantine/core'
+import { theme } from './styles/theme.ts'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './config/i18n.ts'
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={
+      <Center style={{ height: '100vh', width: '100vw' }}>
+        <Loader size="xl" />
+      </Center>
+    }>
+      <MantineProvider theme={theme} defaultColorScheme="auto">
+        <App />
+      </MantineProvider>
+    </Suspense>
   </StrictMode>,
 )
